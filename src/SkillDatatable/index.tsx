9a485882c1590d950f21ddd1065c6e53ea7446ca
Adam Peretti <adam.peretti@gmail.com>
2022-07-09 14:29:54 -0700

    Added predictions to the data table

--- src/SkillDatatable/index.tsx
+++ src/SkillDatatable/index.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren, useMemo } from 'react';
-import { DataGrid, GridRenderCellParams, GridComparatorFn } from '@mui/x-data-grid';
+import { DataGrid, GridRenderCellParams, GridValueFormatterParams } from '@mui/x-data-grid';
 import { SxProps, Theme } from '@mui/system';
-import Skill, {SkillClick} from '../types/Skill'
+import Skill, { SkillClick } from '../types/Skill'
 import './index.css';
 import SkillImage from '../SkillImage';
 
@@ -53,34 +53,47 @@ const sx: SxProps<Theme> = {
   border: "none"
 }
 
-const stringCompare: GridComparatorFn<string> = (v1, v2) => Number(v1) - Number(v2)
+const valueFormatter = (scale: number = 1, decimals: number = 0, suffix='') =>
+  (params: GridValueFormatterParams<number|undefined>) =>
+  params.value === undefined ? `--` : `${(params.value * scale).toFixed(decimals)}${suffix}`
+
 
 function SkillDatatable(props: PropsWithChildren<Props>) {
 
   let { skills, turn, pickSkill } = props
   let round = Math.floor(turn / 10)
-  let formattedSkills = skills.map((skill) => {
-    return {
-      ...skill,
-      avgPick: Number(skill.stats.mean).toFixed(0),
-      pickRate: (Number(skill.stats.pickRate) * 100).toFixed(0),
-      pickRateRounds: (Number(skill.stats.pickRateRounds[round]) * 100).toFixed(0),
-      winRate: (Number(skill.stats.winRate) * 100).toFixed(0),
-      winRateRounds: (Number(skill.stats.winRateRounds[round]) * 100).toFixed(0),
-      survival: (Number(skill.stats.survival[turn]) * 100).toFixed(0),
-      survival5: (Number(skill.stats.survival[turn + 5]) * 100).toFixed(0),
-      survival10: (Number(skill.stats.survival[turn + 10]) * 100).toFixed(0),
-    }
+  let formattedSkills = skills.map((skill) => ({
+    ...skill,
+    avgPick: skill.stats.mean,
+    pickRate: skill.stats.pickRate,
+    pickRateRounds: skill.stats.pickRateRounds[round],
+    winRate: skill.stats.winRate,
+    winRateRounds: skill.stats.winRateRounds[round],
+    survival: skill.stats.survival[turn],
+    survival5: skill.stats.survival[turn + 5],
+    survival10: skill.stats.survival[turn + 10],
+    predictWin: skill.predict?.win,
+    predictGold: skill.predict?.gold,
+    predictDamage: skill.predict?.damage,
+    predictKills: skill.predict?.kills,
+    predictDeaths: skill.predict?.deaths
   })
 
-    let columns = useMemo(() => [
+  )
+
+  let columns = useMemo(() => [
     { field: "abilityName", headerName: "Ability", renderCell: (params: GridRenderCellParams) => <SkillImage skill={params.row} onClick={pickSkill(params.row)} /> },
-    { field: "avgPick", headerName: "Average Pick", sortComparator: stringCompare },
-    { field: "pickRate", headerName: "Percent Picked", sortComparator: stringCompare },
-    { field: "winRate", headerName: "Win Rate", sortComparator: stringCompare },
-    { field: "survival", headerName: "Survival", sortComparator: stringCompare },
-    { field: "survival5", headerName: "Survival 5 Turns", sortComparator: stringCompare },
-    { field: "survival10", headerName: "Survival 10 Turns", sortComparator: stringCompare }
+    { field: "avgPick", headerName: "Average Pick", valueFormatter: valueFormatter(1,0)},
+    { field: "pickRate", headerName: "Percent Picked", valueFormatter: valueFormatter(100,0,'%')},
+    { field: "winRate", headerName: "Win Rate", valueFormatter: valueFormatter(100,0,'%')},
+    { field: "survival", headerName: "Survival", valueFormatter: valueFormatter(100, 0, '%')},
+    { field: "survival5", headerName: "Survival 5 Turns", valueFormatter: valueFormatter(100,0,'%')},
+    { field: "survival10", headerName: "Survival 10 Turns", valueFormatter: valueFormatter(100,0,'%')},
+    { field: "predictWin", headerName: "Win Prediction", valueFormatter: valueFormatter(100,0,'%')},
+    { field: "predictDamage", headerName: "Damage Prediction", valueFormatter: valueFormatter(.001*45,1,'k')},
+    { field: "predictGold", headerName: "Gold Prediction", valueFormatter: valueFormatter(.001*45,1,'k')},
+    { field: "predictKills", headerName: "Kill Prediction", valueFormatter: valueFormatter(45,0)},
+    { field: "predictDeaths", headerName: "Death Prediction", valueFormatter: valueFormatter(45,0)}
   ], [pickSkill])
 
   return (
