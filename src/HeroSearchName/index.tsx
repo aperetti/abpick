2779c6e5b13fd1e3287d4e45cb138139a4348532
Adam Peretti <adam.peretti@gmail.com>
2022-07-29 12:01:42 -0700

    Hover for combos on other heros.

--- src/HeroSearchName/index.tsx
+++ src/HeroSearchName/index.tsx
@@ -1,5 +1,5 @@
 import { Popover2 } from '@blueprintjs/popover2';
-import React, { PropsWithChildren, useEffect, useState } from 'react';
+import React, { PropsWithChildren, MouseEvent, useEffect, useState, useMemo} from 'react';
 import getBestCombos, { ComboResponse } from '../api/getCombos';
 import { NullableSkillList, SkillDict } from '../App';
 import EmptySkillTile from '../EmptySkillTile';
@@ -7,6 +7,7 @@ import SkillImage from '../SkillImage';
 import Skill from '../types/Skill';
 import './index.css';
 import Card from '../Card'
+import { Icon } from '@blueprintjs/core';
 
 interface Props {
   onClick: () => void,
@@ -24,28 +25,25 @@ interface ComboProps {
   allSkills: SkillDict,
 }
 
-function ComboContent({ combos, allSkills }: PropsWithChildren<ComboProps>) {
+function ComboContent({ combos, allSkills}: PropsWithChildren<ComboProps>) {
+
+
+
 
   return (
-    <Card title='Combo'>
-      <div className='hero-search-combo-container'>
-        {combos.reduce<ComboResponse[]>((filteredCombos, combo) => {
-          let foundDuplicate = filteredCombos.find(el => el.skill === combo.skill)
-          if (foundDuplicate === undefined)
-            return [...filteredCombos, combo]
-          else if (foundDuplicate.winPct < combo.winPct)
-            return [...filteredCombos.filter(filterCombo => filterCombo.skill !== combo.skill), combo]
-          else
-            return filteredCombos
-        }, []).sort((el1, el2) => Math.abs(el2.winPct) - el1.winPct).map((el) => {
-          let skill = allSkills[el.skill]
-          let synergy = el.winPct - el.avgWinPct
-          return (
-            <>{Math.abs(synergy) > .03 && <SkillImage synergy={synergy} disableAgs skill={skill} />}</>
-          )
-        })}
-      </div>
-    </Card>
+    <>
+      {combos.length > 0 && <Card title='Combo'>
+        <div className='hero-search-combo-container'>
+          {combos.map((el) => {
+            let skill = allSkills[el.skill]
+            let synergy = el.winPct - el.avgWinPct
+            return (
+              <><SkillImage synergy={synergy} disableAgs skill={skill} /></>
+            )
+          })}
+        </div>
+      </Card>}
+    </>
   )
 }
 
@@ -53,11 +51,15 @@ function HeroSearchName({ onClick, hero, slot, skills, activePick, availableSkil
   let selectedSlot = activePick % 10 === slot && activePick < 40
   let [bestCombos, setBestCombos] = useState<ComboResponse[]>([])
   let [loadingCombos, setLoadingCombos] = useState(false)
+  let [pinCombo, setPinCombo] = useState(false)
+  let [lastSkillString, setLastSkillString] = useState('')
 
   let slotSkills = [0, 1, 2, 3].map(el => {
     let skillSlotIdx = el * 10 + slot
     return skills[skillSlotIdx]
   })
+
+  let skillString = JSON.stringify(slotSkills.map(el => el?.abilityId))
 
   let noPickedSkills = slotSkills.filter(el => el === null).length === 4
 
@@ -70,7 +72,7 @@ function HeroSearchName({ onClick, hero, slot, skills, activePick, availableSkil
         .filter((el): el is Skill => el !== null)
 
       let hasUlt = nonNullSlotSkills
-        .map(el=> isUlt(el.abilityId)).some((el) => el)
+        .map(el => isUlt(el.abilityId)).some((el) => el)
 
       let hasAllSkills = nonNullSlotSkills
         .filter(el => !isUlt(el.abilityId)).length === 3
@@ -89,17 +91,36 @@ function HeroSearchName({ onClick, hero, slot, skills, activePick, availableSkil
       setLoadingCombos(false)
     }
 
-    if (!noPickedSkills && selectedSlot)
+    if (!noPickedSkills && (selectedSlot || lastSkillString !== skillString))
       getCombos()
+    setLastSkillString(skillString)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [activePick])
+  let position = slot % 2 !== 0 ? 'right' : 'left'
+  let openHandler = (event: MouseEvent<HTMLElement>) => {
+    event.stopPropagation()
+    setPinCombo(!pinCombo)
+  }
+  let filteredCombos = useMemo(() => bestCombos.reduce<ComboResponse[]>((filteredCombos, combo) => {
+    let foundDuplicate = filteredCombos.find(el => el.skill === combo.skill)
+    if (foundDuplicate === undefined)
+      return [...filteredCombos, combo]
+    else if (foundDuplicate.winPct < combo.winPct)
+      return [...filteredCombos.filter(filterCombo => filterCombo.skill !== combo.skill), combo]
+    else
+      return filteredCombos
+  }, []), [bestCombos])
 
+  .filter(el => availableSkills.includes(el.skill) && (Math.abs(el.winPct - el.avgWinPct) > .03))
+  .sort((el1, el2) => Math.abs(el2.winPct) - el1.winPct)
   return (
     <div className="hero-name-container" onClick={onClick}>
       <Popover2
         position={slot % 2 !== 0 ? 'right' : 'left'}
-        isOpen={Boolean(!loadingCombos && selectedSlot && bestCombos.length > 0)}
-        content={<ComboContent combos={bestCombos} allSkills={allSkills} />} >
+        disabled={!Boolean(!loadingCombos && filteredCombos.length > 0)}
+        isOpen={(selectedSlot && filteredCombos.length > 0) || undefined}
+        interactionKind='hover'
+        content={<ComboContent combos={filteredCombos} allSkills={allSkills} />} >
         <div className={`hero-name-skills-container ${selectedSlot ? 'hero-name-skills-glow' : ''}`}>
           {slot !== 10 && slot !== 11 && slotSkills.map(skillSlot =>
             (
@@ -108,6 +129,7 @@ function HeroSearchName({ onClick, hero, slot, skills, activePick, availableSkil
             <EmptySkillTile small />
 
           )}
+          {/* {![10, 11].includes(slot) && <Icon onClick={openHandler} className={`hero-search-open-combo-${position}`} icon={pinCombo ? 'star' : 'star-empty'} />} */}
         </div>
       </Popover2>
       <div className='hero-name-text '>
