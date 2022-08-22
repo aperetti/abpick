f33228c3bf0a606b0f8df158969efeb437c232ec
Adam Peretti <adam.peretti@gmail.com>
2022-08-21 21:39:32 -0700

    Add Combos to the Side bar

--- src/SkillTile/index.tsx
+++ src/SkillTile/index.tsx
@@ -1,35 +1,39 @@
 import React, { PropsWithChildren } from 'react';
+import classNames from 'classnames'
 import './index.css';
 import Skill from '../types/Skill';
 import { ContextMenu2 } from '@blueprintjs/popover2';
 import SkillDetails from '../SkillDetails';
 import SkillImage from '../SkillImage';
 import { NullableSkillList } from '../App';
+import { Classes, Icon } from '@blueprintjs/core';
 
 interface Props {
   skill: Skill;
-  skills: NullableSkillList;
+  skills: NullableSkillList
   onClick?: (ctrl: boolean) => void;
-  picked?: boolean;
-  turn: number;
+  picked?: boolean
+  turn: number
+  playerNextTurn: number | undefined
 }
 
-function SkillTile({ skill, onClick, picked, turn, skills}: PropsWithChildren<Props>) {
 
-  let survive = skill?.stats?.survival && skill?.stats?.survival[Math.min(turn + 10, 47)] < .50
-  let win = skill?.stats?.winRate > .5
-  let winCss = !picked && win && survive
-  let surviveCss = !picked && !win && survive
+function SkillTile({ skill, onClick, picked, turn, skills, playerNextTurn}: PropsWithChildren<Props>) {
   let pastDue = turn > skill?.stats?.mean
+  let survival = (playerNextTurn ? skill.stats.survival[playerNextTurn] : 1)
+  let survivalColor = survival < .25 ? "gold" : (survival < .5 ? "silver" : "black")
+
+  let tileClass = classNames("skill", Classes.DARK)
+
 
   return (
-    <div className={`
-      skill bp4-dark ${surviveCss ? 'skill-survive' : ''} ${winCss ? 'skill-win' : ''}`}>
-      {pastDue && <div className='skill-badge'>{Math.round(turn - skill?.stats?.mean)}</div>}
+    <div className={tileClass}>
       <ContextMenu2  content={<SkillDetails skills={skills.filter((el): el is Skill => el !== null)} skill={skill} ></SkillDetails>} >
         <SkillImage onClick={onClick} skill={skill} picked={picked} />
       </ContextMenu2>
-      {/* <SkillImage onClick={onClick} skill={skill} picked={picked} /> */}
+      {!picked && <div className='skill-badge'>
+
+      </div>}
     </div>
   );
 }
