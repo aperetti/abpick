c949aafaef823b1711876a0682df88d2acbd19e5
Adam Peretti <adam.peretti@gmail.com>
2022-09-08 09:16:12 -0700

    Additional notes on mobile viewing

--- src/App.tsx
+++ src/App.tsx
@@ -527,7 +527,7 @@ function App() {
       </DraftBoard>}
       <div className='mobile-support'>
         <Icon icon='warning-sign' size={120} />
-        <H1 color='#a9a9a9'>Ability Draft Plus requires a larger screen</H1>
+        <H1 color='#a9a9a9'>Ability Draft Plus requires a larger screen, if on mobile try rotating your screen.</H1>
       </div>
     </div>
   );
